78334475339659ec639f9859c41f468ad982003a
Jamie McCann <jamie.mccann@umu.se>
2025-04-16 14:54:53 +0200

    adding text input

--- src/components/MembersList.tsx
+++ src/components/MembersList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import membersJson from "../assets/members.json";
 
 interface Member {
@@ -12,25 +13,37 @@ interface Member {
 const members: Member[] = membersJson;
 
 const MembersList = () => {
+  const [inputText, setInputText] = useState<string>("");
+
   return (
     <div>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(event) => {
+          setInputText(event.target.value);
+          console.log(inputText);
+        }}
+      />
       <h2>Members list</h2>
       <ul>
-        {members.map((value, index) => (
-          <li key={index}>
-            <h3>
-              {value.firstName} {value.lastName}
-            </h3>
-            <h4>{value.description}</h4>
-            <ul>
-              {value.interests.map((value2, index2) => (
-                <li key={index2}>
-                  <h5>{value2}</h5>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
+        {members
+          .filter(() => true)
+          .map((value, index) => (
+            <li key={index}>
+              <h3>
+                {value.firstName} {value.lastName}
+              </h3>
+              <h4>{value.description}</h4>
+              <ul>
+                {value.interests.map((value2, index2) => (
+                  <li key={index2}>
+                    <h5>{value2}</h5>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
       </ul>
     </div>
   );
